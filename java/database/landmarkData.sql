992ac34bb5f011f1429ebf35a86a3d807facd6b7
Mohiuddin Kader <kadersql@gmail.com>
2021-02-01 15:11:09 -0500

    database Update

---
+++ java/database/landmarkData.sql
@@ -0,0 +1,31 @@
+insert into landmark (name, description, img, operatingdays, openingtime, closingtime, 
+venuetype, location, rating, address, latitude, longitude)
+values ('Motown Museum', 
+'the nickname given to Motowns first headquarters. 
+A former photographers studio located at 2648 West Grand Boulevard in Detroit', 
+null, 
+'Monday -Friday', 
+'10:00:00', 
+'18:00:00',
+'Museum', 
+'Detroit', 
+'4', 
+'2648 W Grand Blvd, Detroit, MI 48208',
+'42.364141', '-83.088346');
+
+insert into landmark (name, description, img, operatingdays, openingtime, closingtime, 
+venuetype, location, rating, address, latitude, longitude)
+values 
+('Detroit Institute of Arts', 
+'The Detroit Institute of Arts, located in Midtown Detroit, Michigan, 
+has one of the largest and most significant art collections in the United States.', 
+null, 
+'Wednesday -Sunday', 
+'09:00:00', 
+'18:00:00',
+'Museum', 
+'Detroit', 
+'10', 
+'5200 Woodward Ave, Detroit, MI 48202',
+'42.3594', 
+'-83.0645');     
